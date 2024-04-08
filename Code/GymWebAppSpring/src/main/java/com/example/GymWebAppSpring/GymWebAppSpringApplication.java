ca056419e4de1578fb4fc69986b41437610a262f
antBaena <antbaena@uma.es>
2024-04-08 13:59:34 +0200

    el proyecto hombre 2

--- Code/GymWebAppSpring/src/main/java/com/example/GymWebAppSpring/GymWebAppSpringApplication.java
+++ Code/GymWebAppSpring/src/main/java/com/example/GymWebAppSpring/GymWebAppSpringApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.GymWebAppSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class GymWebAppSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(GymWebAppSpringApplication.class, args);
 	}
 
 }
