2ffa039478cfeda56607545142d2f263de525a7c
Eulogio Quemada <elgameshdpay@gmail.com>
2024-04-22 13:15:52 +0200

    Diferenciacion

---
+++ Code/GymWebAppSpring/src/main/java/com/example/GymWebAppSpring/controller/EntrenadorControllerCRUD.java
@@ -0,0 +1,13 @@
+package com.example.GymWebAppSpring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EntrenadorControllerCRUD {
+
+    @GetMapping("/entrenador")
+    public String doInicio() {
+        return "/entrenador/inicio";
+    }
+}
--- Code/GymWebAppSpring/src/main/java/com/example/GymWebAppSpring/controller/EntrenadorControllerCientes.java
+++ Code/GymWebAppSpring/src/main/java/com/example/GymWebAppSpring/controller/EntrenadorControllerCientes.java
@@ -4,10 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class EntrenadorController {
+public class EntrenadorControllerCientes {
 
-    @GetMapping("/entrenador")
-    public String doInicio() {
-        return "/entrenador/inicio";
-    }
 }
