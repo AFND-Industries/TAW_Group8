dd3bf0c3e6d0ed1c1cf846828a6862f01852ad5d
alers16 <aromsan@uma.es>
2024-04-22 13:52:14 +0200

    Update header.js

--- Code/GymWebAppSpring/src/main/resources/static/header.js
+++ Code/GymWebAppSpring/src/main/resources/static/header.js
@@ -2,7 +2,7 @@ function generateHTML() {
     return `
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="/">TAW_Project</a>
+                <a class="navbar-brand" href="/entrenador">TAW_Project</a>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
