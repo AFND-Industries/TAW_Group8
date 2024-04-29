f5ee80578a38f1497607177c0429c5c4a69d2c62
tbms18 <124881416+tbms18@users.noreply.github.com>
2024-04-29 13:45:06 +0200

    database and controllers update

---
+++ Code/GymWebAppSpring/src/main/java/com/example/GymWebAppSpring/controller/admin/SessionController.java
@@ -0,0 +1,45 @@
+package com.example.GymWebAppSpring.controller.admin;
+
+import com.example.GymWebAppSpring.dao.UsuarioRepository;
+import com.example.GymWebAppSpring.entity.Usuario;
+import com.example.GymWebAppSpring.util.HashUtils;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SessionController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    /* ------------------------- Auth Functions */
+    @GetMapping("/login")
+    public String loginPage(){
+        return "auth/login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("dni") String dni, @RequestParam("clave") String password, Model model, HttpSession session) {
+        String passDigest = HashUtils.hashString(password);
+        Usuario usuario = usuarioRepository.findUsuarioByDniAndClave(dni,passDigest);
+        if (usuario != null){
+            session.setAttribute("user",usuario);
+            return "redirect:/";
+        }
+
+        model.addAttribute("error", "El usuario o la contraseña no son válidos");
+        return "auth/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+        return "redirect:/";
+    }
+    /* ------------------------- End Auth Functions */
+}
---
+++ Code/GymWebAppSpring/src/main/webapp/WEB-INF/view/error.jsp
@@ -0,0 +1,40 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%--
+  Created by IntelliJ IDEA.
+  User: tonib
+  Date: 29/04/2024
+  Time: 11:48
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%
+    String error = (String)request.getAttribute("error");
+    Integer code = (Integer) request.getAttribute("error");
+%>
+<html>
+<head>
+    <title>Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS Dependencies -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Bootstrap Icons CSS Dependencies -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+<body>
+<jsp:include page="components/header.jsp" />
+
+<div class="container text-center">
+    <h1 class="text-danger">¡Ups! Ha ocurrido un error</h1>
+    <h3 class="text-danger">Código de error <%=code == null ? "desconocido" : code%></h3>
+    <p class="text-danger">
+        <%=error%>
+    </p>
+</div>
+
+<!-- Bootstrap Javascript Dependencies -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+</html>
