acf615e2061af6bb58b15a1703bdd1ff55a60b51
antBaena <antoniocanetebaena1234@gmail.com>
2024-04-29 14:33:39 +0200

    Update SesionEntrenamientoRepository.java

--- Code/GymWebAppSpring/src/main/java/com/example/GymWebAppSpring/dao/SesionEntrenamientoRepository.java
+++ Code/GymWebAppSpring/src/main/java/com/example/GymWebAppSpring/dao/SesionEntrenamientoRepository.java
@@ -3,6 +3,5 @@ package com.example.GymWebAppSpring.dao;
 import com.example.GymWebAppSpring.entity.Sesionentrenamiento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SesionEntrenamientoRepository extends JpaRepository<Sesionentrenamiento, Integer> {
-
+public interface SesionentrenamientoRepository extends JpaRepository<Sesionentrenamiento, Integer> {
 }
