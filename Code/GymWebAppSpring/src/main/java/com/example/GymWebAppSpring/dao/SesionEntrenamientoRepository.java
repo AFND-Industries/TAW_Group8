a7bcd5a50576060030f311148404ebb668696c0b
Eulogio Quemada <elgameshdpay@gmail.com>
2024-04-29 19:19:29 +0200

    Delete SesionEntrenamientoRepository.java

--- Code/GymWebAppSpring/src/main/java/com/example/GymWebAppSpring/dao/SesionEntrenamientoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.GymWebAppSpring.dao;
-
-import com.example.GymWebAppSpring.entity.Sesionentrenamiento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SesionentrenamientoRepository extends JpaRepository<Sesionentrenamiento, Integer> {
-}
