8ee78860218fb5e9c48447d9659ed8e0b8200f4f
antBaena <antoniocanetebaena1234@gmail.com>
2024-05-05 17:48:09 +0200

    cliente update se guardan datos y he roto mi intij

--- Code/GymWebAppSpring/src/main/java/com/example/GymWebAppSpring/controller/ClienteCotroller.java
+++ Code/GymWebAppSpring/src/main/java/com/example/GymWebAppSpring/controller/ClienteCotroller.java
@@ -5,6 +5,9 @@ import com.example.GymWebAppSpring.dao.RutinaUsuarioRepository;
 import com.example.GymWebAppSpring.dao.SesionentrenamientoRepository;
 import com.example.GymWebAppSpring.entity.*;
 import com.example.GymWebAppSpring.util.AuthUtils;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -77,4 +80,37 @@ public class ClienteCotroller {
         modelo.addAttribute("ejercicioElegido",ejercicios.getFirst());
         return "client/verSesion";
     }
+
+//    @PostMapping("/valorarEntrenamiento")
+//    public String doValorarEntrenamiento(@RequestParam("datosEntrenamiento") String datos) {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//
+//        // Convertir el JSON en un árbol de nodos
+//        try {
+//            JsonNode jsonNode = objectMapper.readTree(datos);
+//            System.out.println("JSON: " + jsonNode);
+//            for(JsonNode nodo : jsonNode){
+//                JsonNode repeticionesNode = nodo.get("repeticiones");
+//                JsonNode mpesoNode = nodo.get("mpeso");
+//
+//                // Verifica si los nodos no son nulos antes de llamar a asText()
+//                if(repeticionesNode != null && mpesoNode != null) {
+//                    String repeticiones = repeticionesNode.asText();
+//                    String mpeso = mpesoNode.asText();
+//
+//                    System.out.println("Nodo:");
+//                    System.out.println("Repeticiones: " + repeticiones);
+//                    System.out.println("Mpeso: " + mpeso);
+//                } else {
+//                    System.err.println("Alguno de los nodos es nulo.");
+//                }
+//            }
+//        } catch (JsonProcessingException e) {
+//            System.err.println("Error al procesar el JSON: " + e.getMessage());
+//        }
+//
+//
+//
+//        return "redirect:/";
+//    }
 }
