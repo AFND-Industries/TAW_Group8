0bc8d71da30166db87568442fa243ce2e58f3d0c
tbms18 <124881416+tbms18@users.noreply.github.com>
2024-06-17 17:40:11 +0200

    removing database schema creator

--- Code/GymWebAppSpring/src/DATOSOFICIALES.sql
+++ Code/GymWebAppSpring/src/DATOSOFICIALES.sql
@@ -19,19 +19,6 @@
 -- Table structure for table `categoria`
 --
 
-DROP TABLE IF EXISTS `categoria`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `categoria` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `NOMBRE` varchar(32) NOT NULL,
-  `DESCRIPCION` varchar(256) NOT NULL,
-  `TIPOS_BASE` varchar(64) NOT NULL,
-  `ICONO` varchar(256) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
 --
 -- Dumping data for table `categoria`
 --
@@ -46,16 +33,6 @@ UNLOCK TABLES;
 -- Table structure for table `dificultad`
 --
 
-DROP TABLE IF EXISTS `dificultad`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `dificultad` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `NOMBRE` varchar(32) NOT NULL,
-  `LOGO` varchar(256) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `dificultad`
@@ -71,31 +48,6 @@ UNLOCK TABLES;
 -- Table structure for table `ejercicio`
 --
 
-DROP TABLE IF EXISTS `ejercicio`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `ejercicio` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `NOMBRE` varchar(32) NOT NULL,
-  `DESCRIPCION` varchar(256) NOT NULL,
-  `MUSCULO` int NOT NULL,
-  `EQUIPAMIENTO` varchar(32) NOT NULL,
-  `TIPOFUERZA` int NOT NULL,
-  `MUSCULO_SECUNDARIO` int DEFAULT NULL,
-  `VIDEO` varchar(256) NOT NULL,
-  `LOGO` varchar(256) NOT NULL,
-  `CATEGORIA` int NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `EJERCICIO_MUSCULO_ID_fk` (`MUSCULO`),
-  KEY `EJERCICIO_MUSCULO_ID_fk_2` (`MUSCULO_SECUNDARIO`),
-  KEY `EJERCICIO_TIPOFUERZA_ID_fk` (`TIPOFUERZA`),
-  KEY `EJERCICIO_ibfk_1` (`CATEGORIA`),
-  CONSTRAINT `EJERCICIO_ibfk_1` FOREIGN KEY (`CATEGORIA`) REFERENCES `categoria` (`ID`),
-  CONSTRAINT `EJERCICIO_MUSCULO_ID_fk` FOREIGN KEY (`MUSCULO`) REFERENCES `musculo` (`ID`),
-  CONSTRAINT `EJERCICIO_MUSCULO_ID_fk_2` FOREIGN KEY (`MUSCULO_SECUNDARIO`) REFERENCES `musculo` (`ID`),
-  CONSTRAINT `EJERCICIO_TIPOFUERZA_ID_fk` FOREIGN KEY (`TIPOFUERZA`) REFERENCES `tipofuerza` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `ejercicio`
@@ -111,22 +63,6 @@ UNLOCK TABLES;
 -- Table structure for table `ejerciciosesion`
 --
 
-DROP TABLE IF EXISTS `ejerciciosesion`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `ejerciciosesion` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `ORDEN` int NOT NULL,
-  `ESPECIFICACIONES` varchar(256) NOT NULL COMMENT 'JSON syntax',
-  `SESIONENTRENAMIENTO_ID` int NOT NULL,
-  `EJERCICIO_ID` int NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `EJERCICIOSESION_ibfk_1` (`EJERCICIO_ID`),
-  KEY `EJERCICIOSESION_ibfk_2` (`SESIONENTRENAMIENTO_ID`),
-  CONSTRAINT `EJERCICIOSESION_ibfk_1` FOREIGN KEY (`EJERCICIO_ID`) REFERENCES `ejercicio` (`ID`),
-  CONSTRAINT `EJERCICIOSESION_ibfk_2` FOREIGN KEY (`SESIONENTRENAMIENTO_ID`) REFERENCES `sesionentrenamiento` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `ejerciciosesion`
@@ -140,20 +76,7 @@ UNLOCK TABLES;
 
 --
 -- Table structure for table `entrenadorasignado`
---
 
-DROP TABLE IF EXISTS `entrenadorasignado`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `entrenadorasignado` (
-  `ENTRENADOR` int NOT NULL,
-  `CLIENTE` int NOT NULL,
-  PRIMARY KEY (`ENTRENADOR`,`CLIENTE`),
-  KEY `ENTRENADORASIGNADO_USUARIO_ID_fk_2` (`CLIENTE`),
-  CONSTRAINT `ENTRENADORASIGNADO_USUARIO_ID_fk` FOREIGN KEY (`ENTRENADOR`) REFERENCES `usuario` (`ID`),
-  CONSTRAINT `ENTRENADORASIGNADO_USUARIO_ID_fk_2` FOREIGN KEY (`CLIENTE`) REFERENCES `usuario` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `entrenadorasignado`
@@ -169,21 +92,6 @@ UNLOCK TABLES;
 -- Table structure for table `informacionejercicio`
 --
 
-DROP TABLE IF EXISTS `informacionejercicio`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `informacionejercicio` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `EVALUACION` varchar(256) NOT NULL COMMENT 'JSON syntax',
-  `EJERCICIOSESION_ID` int NOT NULL,
-  `INFORMACIONSESION_ID` int NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `INFORMACIONEJERCICIO_ibfk_1` (`EJERCICIOSESION_ID`),
-  KEY `INFORMACIONEJERCICIO_ibfk_2` (`INFORMACIONSESION_ID`),
-  CONSTRAINT `INFORMACIONEJERCICIO_ibfk_1` FOREIGN KEY (`EJERCICIOSESION_ID`) REFERENCES `ejerciciosesion` (`ID`),
-  CONSTRAINT `INFORMACIONEJERCICIO_ibfk_2` FOREIGN KEY (`INFORMACIONSESION_ID`) REFERENCES `informacionsesion` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `informacionejercicio`
@@ -199,24 +107,6 @@ UNLOCK TABLES;
 -- Table structure for table `informacionsesion`
 --
 
-DROP TABLE IF EXISTS `informacionsesion`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `informacionsesion` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `VALORACION` int DEFAULT NULL,
-  `COMENTARIO` varchar(256) DEFAULT NULL,
-  `FECHA_FIN` date DEFAULT NULL,
-  `SESIONENTRENAMIENTO_ID` int NOT NULL,
-  `USUARIO_ID` int NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `INFORMACIONSESION_USUARIO_2` (`USUARIO_ID`),
-  KEY `INFORMACIONSESION_ibfk_1` (`SESIONENTRENAMIENTO_ID`),
-  CONSTRAINT `INFORMACIONSESION_ibfk_1` FOREIGN KEY (`SESIONENTRENAMIENTO_ID`) REFERENCES `sesionentrenamiento` (`ID`),
-  CONSTRAINT `INFORMACIONSESION_USUARIO_2` FOREIGN KEY (`USUARIO_ID`) REFERENCES `usuario` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
 --
 -- Dumping data for table `informacionsesion`
 --
@@ -231,17 +121,6 @@ UNLOCK TABLES;
 -- Table structure for table `musculo`
 --
 
-DROP TABLE IF EXISTS `musculo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `musculo` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `NOMBRE` varchar(32) NOT NULL,
-  `DESCRIPCION` varchar(256) NOT NULL,
-  `IMAGEN` varchar(256) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `musculo`
@@ -256,25 +135,6 @@ UNLOCK TABLES;
 --
 -- Table structure for table `rutina`
 --
-
-DROP TABLE IF EXISTS `rutina`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `rutina` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `NOMBRE` varchar(32) NOT NULL,
-  `DESCRIPCION` varchar(256) NOT NULL,
-  `DIFICULTAD` int NOT NULL,
-  `FECHA_CREACION` date NOT NULL,
-  `ENTRENADOR` int NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `RUTINA_DIFICULTADRUTINA_DIFICULTAD_fk` (`DIFICULTAD`),
-  KEY `RUTINA_USUARIO_FK` (`ENTRENADOR`),
-  CONSTRAINT `RUTINA_DIFICULTADRUTINA_DIFICULTAD_fk` FOREIGN KEY (`DIFICULTAD`) REFERENCES `dificultad` (`ID`),
-  CONSTRAINT `RUTINA_USUARIO_FK` FOREIGN KEY (`ENTRENADOR`) REFERENCES `usuario` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
 --
 -- Dumping data for table `rutina`
 --
@@ -289,19 +149,6 @@ UNLOCK TABLES;
 -- Table structure for table `rutinacliente`
 --
 
-DROP TABLE IF EXISTS `rutinacliente`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `rutinacliente` (
-  `USUARIO_ID` int NOT NULL,
-  `RUTINA_ID` int NOT NULL,
-  `FECHA_INICIO` date NOT NULL,
-  PRIMARY KEY (`USUARIO_ID`,`RUTINA_ID`),
-  KEY `RUTINACLIENTE_RUTINA_1` (`RUTINA_ID`),
-  CONSTRAINT `RUTINACLIENTE_RUTINA_1` FOREIGN KEY (`RUTINA_ID`) REFERENCES `rutina` (`ID`),
-  CONSTRAINT `RUTINACLIENTE_USUARIO_2` FOREIGN KEY (`USUARIO_ID`) REFERENCES `usuario` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `rutinacliente`
@@ -317,20 +164,6 @@ UNLOCK TABLES;
 -- Table structure for table `sesionentrenamiento`
 --
 
-DROP TABLE IF EXISTS `sesionentrenamiento`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `sesionentrenamiento` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `NOMBRE` varchar(32) NOT NULL,
-  `DESCRIPCION` varchar(256) NOT NULL,
-  `DIA` int NOT NULL,
-  `RUTINA` int NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `rutina_id___fk` (`RUTINA`),
-  CONSTRAINT `rutina_id___fk` FOREIGN KEY (`RUTINA`) REFERENCES `rutina` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `sesionentrenamiento`
@@ -346,16 +179,6 @@ UNLOCK TABLES;
 -- Table structure for table `tipofuerza`
 --
 
-DROP TABLE IF EXISTS `tipofuerza`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tipofuerza` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `NOMBRE` varchar(32) NOT NULL,
-  `DESCRIPCION` varchar(256) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `tipofuerza`
@@ -370,17 +193,6 @@ UNLOCK TABLES;
 --
 -- Table structure for table `tipousuario`
 --
-
-DROP TABLE IF EXISTS `tipousuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tipousuario` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `NOMBRE` varchar(32) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
 --
 -- Dumping data for table `tipousuario`
 --
@@ -395,23 +207,6 @@ UNLOCK TABLES;
 -- Table structure for table `usuario`
 --
 
-DROP TABLE IF EXISTS `usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usuario` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `NOMBRE` varchar(32) NOT NULL,
-  `APELLIDOS` varchar(32) NOT NULL,
-  `GENERO` char(1) NOT NULL,
-  `EDAD` int NOT NULL,
-  `DNI` varchar(32) NOT NULL,
-  `CLAVE` varchar(64) NOT NULL,
-  `TIPO` int NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `USUARIO_TIPOUSUARIO_1` (`TIPO`),
-  CONSTRAINT `USUARIO_TIPOUSUARIO_1` FOREIGN KEY (`TIPO`) REFERENCES `tipousuario` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `usuario`
@@ -419,17 +214,8 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'Juan','Pérez','M',28,'1','6B86B273FF34FCE19D6B804EFF5A3F5747ADA4EAA22F1D49C01E52DDB7875B4B',1),(2,'Ana','Martínez','F',32,'2','D4735E3A265E16EEE03F59718B9B5D03019C07D8B6C51F90DA3A666EEC13AB35',2),(3,'Carlos','López','M',25,'11223344X','6D5074B4BF2B913866157D7674F1EDA042C5C614876DE876F7512702D2572A06',1),(4,'Francisco','Domínguez','M',32,'22222222A','6D5074B4BF2B913866157D7674F1EDA042C5C614876DE876F7512702D2572A06',1);
+INSERT INTO `usuario` VALUES (1,'Juan','Pérez','M',28,'1','6B86B273FF34FCE19D6B804EFF5A3F5747ADA4EAA22F1D49C01E52DDB7875B4B',1),(2,'Ana','Martínez','F',32,'2','D4735E3A265E16EEE03F59718B9B5D03019C07D8B6C51F90DA3A666EEC13AB35',2),(3,'Carlos','López','M',25,'11223344X','6D5074B4BF2B913866157D7674F1EDA042C5C614876DE876F7512702D2572A06',1),(4,'Francisco','Domínguez','M',32,'22222222A','6D5074B4BF2B913866157D7674F1EDA042C5C614876DE876F7512702D2572A06',3);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2024-06-16 23:19:43
