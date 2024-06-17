73545ef27c04d50e832ae19c456a2d267b561497
Eulogio Quemada <elgameshdpay@gmail.com>
2024-06-17 19:52:45 +0200

    EjercicioService

--- Code/GymWebAppSpring/src/main/java/com/example/GymWebAppSpring/dao/EjercicioRepository.java
+++ Code/GymWebAppSpring/src/main/java/com/example/GymWebAppSpring/dao/EjercicioRepository.java
@@ -1,5 +1,6 @@
 package com.example.GymWebAppSpring.dao;
 
+import com.example.GymWebAppSpring.dto.MusculoDTO;
 import com.example.GymWebAppSpring.entity.Categoria;
 import com.example.GymWebAppSpring.entity.Ejercicio;
 import com.example.GymWebAppSpring.entity.Musculo;
@@ -12,8 +13,8 @@ import java.util.List;
 
 public interface EjercicioRepository extends JpaRepository<Ejercicio, Integer> {
 
-    @Query("select e from Ejercicio e, Ejerciciosesion es, Sesionentrenamiento s where es.ejercicio = e and es.sesionentrenamiento = s and s = :sesion")
-    List<Ejercicio> findEjercicioBySesion(@Param("sesion") Sesionentrenamiento sesion);
+    @Query("select e from Ejercicio e, Ejerciciosesion es, Sesionentrenamiento s where es.ejercicio = e and es.sesionentrenamiento = s and s.id = :sesionId")
+    List<Ejercicio> findEjercicioBySesion(@Param("sesionId") Integer sesionId);
 
     @Query("SELECT e FROM Ejercicio e WHERE e.id IN :ids")
     List<Ejercicio> findEjercicioByIds(@Param("ids") List<Integer> lista);
@@ -21,11 +22,11 @@ public interface EjercicioRepository extends JpaRepository<Ejercicio, Integer> {
     @Query("select e from Ejercicio e where e.categoria.id = 1")
     List<Ejercicio> findAllEjerciciosFuerza();
 
-    @Query("SELECT e FROM Ejercicio e WHERE e.categoria = :categoria")
-    List<Ejercicio> findAllByCategoria(Categoria categoria);
+    @Query("SELECT e FROM Ejercicio e WHERE e.categoria.id = :categoriaId")
+    List<Ejercicio> findAllByCategoria(Integer categoriaId);
 
-    @Query("SELECT e FROM Ejercicio e WHERE e.musculo = :musculo")
-    List<Ejercicio> findAllByMusculo(Musculo musculo);
+    @Query("SELECT e FROM Ejercicio e WHERE e.musculo.id = :musculoId")
+    List<Ejercicio> findAllByMusculo(Integer musculoId);
 
     @Query("SELECT e FROM Ejercicio e WHERE e.nombre like %:nombre%")
     List<Ejercicio> findAllByNombre(String nombre);
---
+++ Code/GymWebAppSpring/src/main/java/com/example/GymWebAppSpring/service/EjercicioService.java
@@ -0,0 +1,86 @@
+package com.example.GymWebAppSpring.service;
+
+import com.example.GymWebAppSpring.dao.CategoriaRepository;
+import com.example.GymWebAppSpring.dao.EjercicioRepository;
+import com.example.GymWebAppSpring.dao.MusculoRepository;
+import com.example.GymWebAppSpring.dao.TipoFuerzaRepository;
+import com.example.GymWebAppSpring.dto.EjercicioDTO;
+import com.example.GymWebAppSpring.entity.Ejercicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EjercicioService extends DTOService<EjercicioDTO, Ejercicio>{
+
+    @Autowired
+    protected EjercicioRepository ejercicioRepository;
+
+    @Autowired
+    protected CategoriaRepository categoriaRepository;
+
+    @Autowired
+    protected MusculoRepository musculoRepository;
+
+    @Autowired
+    protected TipoFuerzaRepository tipoFuerzaRepository;
+
+    public List<EjercicioDTO> findAll() {
+        List<Ejercicio> lista = this.ejercicioRepository.findAll();
+        return this.entidadesADTO(lista);
+    }
+
+    List<EjercicioDTO> findEjercicioBySesion(Integer sesionId) {
+        List<Ejercicio> lista = this.ejercicioRepository.findEjercicioBySesion(sesionId);
+        return this.entidadesADTO(lista);
+    }
+
+    List<EjercicioDTO> findEjercicioByIds(List<Integer> list) {
+        List<Ejercicio> lista = this.ejercicioRepository.findEjercicioByIds(list);
+        return this.entidadesADTO(lista);
+    }
+
+    List<EjercicioDTO> findAllEjerciciosFuerza() {
+        List<Ejercicio> lista = this.ejercicioRepository.findAllEjerciciosFuerza();
+        return this.entidadesADTO(lista);
+    }
+
+    List<EjercicioDTO> findAllByCategoria(Integer categoriaId) {
+        List<Ejercicio> lista = this.ejercicioRepository.findAllByCategoria(categoriaId);
+        return this.entidadesADTO(lista);
+    }
+
+    List<EjercicioDTO> findAllByMusculo(Integer musculoId) {
+        List<Ejercicio> lista = this.ejercicioRepository.findAllByMusculo(musculoId);
+        return this.entidadesADTO(lista);
+    }
+
+    List<EjercicioDTO> findAllByNombre(String nombre) {
+        List<Ejercicio> lista = this.ejercicioRepository.findAllByNombre(nombre);
+        return this.entidadesADTO(lista);
+    }
+
+    public EjercicioDTO findById(Integer ejercicioId) {
+        Ejercicio ejercicio = this.ejercicioRepository.findById(ejercicioId).orElse(null);
+        return ejercicio == null ? null : ejercicio.toDTO();
+    }
+
+    public void save(EjercicioDTO ejercicioDTO) {
+        Ejercicio ejercicio = this.ejercicioRepository.findById(ejercicioDTO.getId()).orElse(new Ejercicio());
+        ejercicio.setDescripcion(ejercicioDTO.getDescripcion());
+        ejercicio.setCategoria(this.categoriaRepository.findById(ejercicioDTO.getCategoria().getId()).orElse(null));
+        ejercicio.setNombre(ejercicioDTO.getNombre());
+        ejercicio.setLogo(ejercicioDTO.getLogo());
+        ejercicio.setEquipamiento(ejercicioDTO.getEquipamiento());
+        ejercicio.setMusculo(this.musculoRepository.findById(ejercicioDTO.getMusculo().getId()).orElse(null));
+        ejercicio.setMusculoSecundario(this.musculoRepository.findById(ejercicioDTO.getMusculoSecundario().getId()).orElse(null));
+        ejercicio.setTipofuerza(this.tipoFuerzaRepository.findById(ejercicioDTO.getTipofuerza().getId()).orElse(null));
+        ejercicio.setVideo(ejercicioDTO.getVideo());
+        this.ejercicioRepository.save(ejercicio);
+    }
+
+    public void delete(Integer ejercicioId) {
+        this.ejercicioRepository.deleteById(ejercicioId);
+    }
+}
