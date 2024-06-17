5bd016bffd527e6b81fed78ae5b7128c93a6de2b
Eulogio Quemada <elgameshdpay@gmail.com>
2024-06-17 18:41:07 +0200

    DTOService

---
+++ Code/GymWebAppSpring/src/main/java/com/example/GymWebAppSpring/service/RutinaService.java
@@ -0,0 +1,7 @@
+package com.example.GymWebAppSpring.service;
+
+import com.example.GymWebAppSpring.entity.Rutina;
+import com.example.GymWebAppSpring.dto.RutinaDTO;
+
+public class RutinaService extends DTOService<RutinaDTO, Rutina>{
+}
