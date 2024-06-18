14233c4d5a60e78c22cc0cc7091b30292e2aee20
Eulogio Quemada <elgameshdpay@gmail.com>
2024-06-18 19:36:18 +0200

    Fix

--- Code/GymWebAppSpring/src/main/java/com/example/GymWebAppSpring/dao/RutinaClienteIDRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.GymWebAppSpring.dao;
-
-import com.example.GymWebAppSpring.entity.RutinaclienteId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RutinaClienteIDRepository extends JpaRepository<RutinaclienteId, Integer> {
-}
